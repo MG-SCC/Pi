7873c9ef14ce69806e8e97ec7e6ae5deb156497f
Matthew Gabrielle <w0709748@myscc.ca>
2024-06-19 23:30:55 -0400

    Full commit

---
+++ Data/PiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pi.Models;
+
+namespace Pi.Data
+{
+    public class PiContext : DbContext
+    {
+        public PiContext (DbContextOptions<PiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pi.Models.Furniture> Furniture { get; set; } = default!;
+    }
+}
---
+++ README.txt
@@ -0,0 +1,19 @@
+2024-06-18
+2230
+Created README.txt
+
+2240
+Added controller
+
+2255
+Added view
+
+2330
+Added model
+
+2024-06-19
+0640
+Created database
+
+0750
+Created database
